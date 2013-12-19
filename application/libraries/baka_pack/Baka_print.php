de7d39a7d556048ccb57b8084bd0e361dea19ab1
Fery Wardiyanto <ferywardiyanto@gmail.com>
2013-12-20 05:04:33 +0700

    adding licenses in top of each baka_pack library

--- application/libraries/baka_pack/Baka_print.php
+++ application/libraries/baka_pack/Baka_print.php
@@ -1,6 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
+ * CodeIgniter Baka Pack
+ *
+ * My very own Codeigniter core library that used on all of my projects
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Open Software License version 3.0
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/OSL-3.0
+ *
+ * @package     Baka_pack
+ * @author      Fery Wardiyanto
+ * @copyright   Copyright (c) Fery Wardiyanto. (ferywardiyanto@gmail.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @since       Version 0.1
+ */
+
+// -----------------------------------------------------------------------------
+
+/**
  * BAKA Printing Class
  *
  * @package		Baka_pack
