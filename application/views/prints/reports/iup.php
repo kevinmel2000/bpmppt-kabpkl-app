ad1101f076f3dad4284302de577d9e1abea56398
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-01-15 09:14:46 +0700

    fixing bpmppt_iup report view

--- application/views/prints/reports/iup.php
+++ application/views/prints/reports/iup.php
@@ -2,19 +2,13 @@
 	<thead>
 		<tr>
 			<th>No.</th>
-			<th>Nama Perusahaan</th>
-			<th>Alamat Perusahaan</th>
 			<th>Nama Pemohon</th>
-			<th>Nomor IUI</th>
-			<th>Berlaku Mulai</th>
-			<th>Berlaku Sp. Dengan</th>
-			<th>Komoditi</th>
-			<th>Total Investasi</th>
-			<th>Jenis Industri</th>
-			<th>Kapasitas Industri</th>
-			<th>KBLI</th>
-			<th colspan="2">Naker</th>
-			<th>Skala Perusahaan</th>
+			<th>Alamat Pemohon</th>
+			<th>Lokasi</th>
+			<th>Nomor Izin</th>
+			<th>Tanggal Terbit</th>
+			<th>Tanggal Berlaku</th>
+			<th>Keterangan</th>
 		</tr>
 		<tr>
 			<th>1</th>
@@ -25,32 +19,19 @@
 			<th>6</th>
 			<th>7</th>
 			<th>8</th>
-			<th>9</th>
-			<th>10</th>
-			<th>11</th>
-			<th>12</th>
-			<th>13</th>
-			<th>14</th>
-			<th>15</th>
 		</tr>
 	</thead>
 	<tbody>
 	<?php if ( $results ) : $i = 1; foreach( $results as $row ) : ?>
 		<tr id="baris-<?php echo $row->id ?>" style="text-transform: uppercase;">
 			<td class="align-center"><?php echo $i ?></td>
-			<td class="align-center"><?php echo $row->no_agenda ?></td>
-			<td class="align-left"><?php echo $row->usaha_jenis ?></td>
-			<td class="align-center"><?php echo $row->no_agenda ?></td>
-			<td class="align-left"><?php echo $row->usaha_nama ?></td>
-			<td class="align-left"><?php echo $row->usaha_alamat ?></td>
 			<td class="align-left"><?php echo $row->pemohon_nama ?></td>
-			<td class="align-left"><?php echo $row->usaha_status ?></td>
-			<td class="align-left"><?php echo $row->usaha_kegiatan_pokok ?></td>
-			<td class="align-center"><?php echo $row->usaha_kegiatan_kbli ?></td>
-			<td class="align-center"><?php echo $row->pembaruan_ke ?></td>
-			<td class="align-left"><?php echo $row->pengajuan_jenis ?></td>
+			<td class="align-left"><?php echo $row->pemohon_alamat ?></td>
+			<td class="align-center"><?php echo $row->usaha_lokasi ?></td>
+			<td class="align-center"><?php echo $row->no_agenda ?></td>
 			<td class="align-center"><?php echo bdate( 'd F Y', $row->created_on ) ?></td>
 			<td class="align-center"><?php echo bdate( 'd F Y', $row->created_on ) ?></td>
+			<td class="align-left"><?php echo '-' ?></td>
 		</tr>
 	<?php $i++; endforeach; else : ?>
 		<tr>
