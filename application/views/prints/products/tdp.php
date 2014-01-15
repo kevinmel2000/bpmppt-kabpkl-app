026c8148c2d5d759ab83e56a434d2bde226137e0
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-01-16 02:40:10 +0700

    re-fixing bpmppt_tdp product view

--- application/views/prints/products/tdp.php
+++ application/views/prints/products/tdp.php
@@ -12,22 +12,15 @@
 		<p style="font-family: 'Arial'">BERDASARKAN<br>
 UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 3 TAHUN 1982<br>
 TENTANG WAJIB DAFTAR PERUSAHAAN<br>
-<?php if( strtolower($usaha_jenis) == 'koprasi' ) : ?>
+<?php // koprasi ---------------------------------------------------------------
+if( strtolower($usaha_jenis) == 'koprasi' ) : ?>
 UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 25 TAHUN 1992<br>
-TENTANG PERKOPERASIAN</p>
-<?php elseif( strtolower($usaha_jenis) == 'badan usaha milik negara (bumn)' ) : ?>
-UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 3 TAHUN 1982<br>
-TENTANG WAJIB DAFTAR PERUSAHAAN<br>
-<?php elseif( strtolower($usaha_jenis) == 'Perorangan (PO)' ) : ?>
-UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 3 TAHUN 1982<br>
-TENTANG WAJIB DAFTAR PERUSAHAAN<br>
-<?php elseif( strtolower($usaha_jenis) == 'Perseroan Komanditer (CV)' ) : ?>
-UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 3 TAHUN 1982<br>
-TENTANG WAJIB DAFTAR PERUSAHAAN<br>
-<?php elseif( strtolower($usaha_jenis) == 'Perseroan Terbatas (PT)' ) : ?>
+TENTANG PERKOPERASIAN
+<?php // Perseroan Terbatas (PT) -----------------------------------------------
+elseif( strtolower($usaha_jenis) == 'Perseroan Terbatas (PT)' ) : ?>
 UNDANG-UNDANG REPUBLIK INDONESIA NOMOR 40 TAHUN 2007<br>
-TENTANG PERSEROAN TERBATAS<br>
-<?php endif; ?>
+TENTANG PERSEROAN TERBATAS
+<?php endif; ?></p>
 	</td>
 </tr>
 <tr><td colspan="6" style="width:100%">&nbsp;</td></tr>
@@ -76,8 +69,16 @@ TENTANG PERSEROAN TERBATAS<br>
 	<td class="align-center" style="width:20%; border-left: 1px solid #000; border-right: 2px solid #000;">KBLI :<br><?php echo strtoupper($usaha_kbli) ?></td>
 </tr>
 
-<?php if( strtolower($usaha_jenis) == 'koprasi' ) : ?>
 <tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
+<?php // koprasi ---------------------------------------------------------------
+if( strtolower($usaha_jenis) == 'koprasi' ) : ?>
+	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI NEGARA KOPERASI DAN USAHA KECIL DAN MENENGAH REPUBLIK INDONESIA ATAS AKTA PENDIRIAN KOPERASI</td>
+</tr>
+<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
+	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : 518/9436.a/BH/XI2006</td>
+	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 23 NOVEMBER 2006</td>
+<?php // Perseroan Terbatas (PT) -----------------------------------------------
+elseif( strtolower($usaha_jenis) == 'Perseroan Terbatas (PT)' ) : ?>
 	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI KEHAKIMAN</td>
 </tr>
 <tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
@@ -97,96 +98,8 @@ TENTANG PERSEROAN TERBATAS<br>
 <tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
 	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : AHU-50221. AH. O1. 02. TH. 2008</td>
 	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2008</td>
-</tr>
-<?php elseif( strtolower($usaha_jenis) == 'badan usaha milik negara (bumn)' ) : ?>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI KEHAKIMAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C2-8679.HT 01 01 TH. 2001</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 8 AGUSTUS 2001</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN MENTERI KEHAKIMAN ATAS AKTA PERUBAHAN ANGGARAN DASAR</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C-22542. AH. 01. 04 TH. 2005</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2005</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN METNERI HUKUM DAN HAM REPUBLIK INDONESIA ATAS AKTA PERUBAHAN ANGGARAN DASAR PERSEROAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : AHU-50221. AH. O1. 02. TH. 2008</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2008</td>
-</tr>
-<?php elseif( strtolower($usaha_jenis) == 'Perorangan (PO)' ) : ?>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI KEHAKIMAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C2-8679.HT 01 01 TH. 2001</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 8 AGUSTUS 2001</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN MENTERI KEHAKIMAN ATAS AKTA PERUBAHAN ANGGARAN DASAR</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C-22542. AH. 01. 04 TH. 2005</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2005</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN METNERI HUKUM DAN HAM REPUBLIK INDONESIA ATAS AKTA PERUBAHAN ANGGARAN DASAR PERSEROAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : AHU-50221. AH. O1. 02. TH. 2008</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2008</td>
-</tr>
-<?php elseif( strtolower($usaha_jenis) == 'Perseroan Komanditer (CV)' ) : ?>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI KEHAKIMAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C2-8679.HT 01 01 TH. 2001</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 8 AGUSTUS 2001</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN MENTERI KEHAKIMAN ATAS AKTA PERUBAHAN ANGGARAN DASAR</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C-22542. AH. 01. 04 TH. 2005</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2005</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN METNERI HUKUM DAN HAM REPUBLIK INDONESIA ATAS AKTA PERUBAHAN ANGGARAN DASAR PERSEROAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : AHU-50221. AH. O1. 02. TH. 2008</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2008</td>
-</tr>
-<?php elseif( strtolower($usaha_jenis) == 'Perseroan Terbatas (PT)' ) : ?>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PENGESAHAN MENTERI KEHAKIMAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C2-8679.HT 01 01 TH. 2001</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 8 AGUSTUS 2001</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN MENTERI KEHAKIMAN ATAS AKTA PERUBAHAN ANGGARAN DASAR</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom-width: 1px;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : C-22542. AH. 01. 04 TH. 2005</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2005</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0; border-bottom: 1px solid #ccc">
-	<td colspan="6" style="width:100%; border-right: 2px solid #000;" class="bold underline">PERSETUJUAN METNERI HUKUM DAN HAM REPUBLIK INDONESIA ATAS AKTA PERUBAHAN ANGGARAN DASAR PERSEROAN</td>
-</tr>
-<tr style="font-family: 'Arial'; border: 2px solid #000; border-top-width: 0;">
-	<td colspan="4" style="width:70%; border-right: 1px solid #ccc;">NOMOR : AHU-50221. AH. O1. 02. TH. 2008</td>
-	<td colspan="2" style="width:30%; border-right: 2px solid #000;">TANGGAL : 12 AGUSTUS 2008</td>
-</tr>
 <?php endif; ?>
+</tr>
 
 <tr><td colspan="6" style="width:100%">&nbsp;</td></tr>
 <tr style="font-family: 'Arial'">
