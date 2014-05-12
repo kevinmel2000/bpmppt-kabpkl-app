425be09e17e6768049332bfa779f4148f9abcbd1
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-05-13 04:19:19 +0700

    try out something :see_no_evil:

--- Gruntfile.js
+++ Gruntfile.js
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
   }
 
   // grunt.registerTask('watch', ['watch']);
-  grunt.registerTask('test', testSubtasks);
+  grunt.registerTask('testdist', ['csstest', 'phptest']);
 
   grunt.registerTask('phptest', ['phplint', 'phpunit']);
   grunt.registerTask('csstest', ['less', 'autoprefixer', 'csscomb', 'csslint', 'cssmin']);
