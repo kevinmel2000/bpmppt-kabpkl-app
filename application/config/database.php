43679e12bd1fcb82c40c52ffb549b7536cef7257
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-05-13 04:41:57 +0700

    try to add database to travis env

--- application/config/database.php
+++ application/config/database.php
@@ -8,16 +8,23 @@
 if (VCAP_SERVICES)
 {
 	$service_json = json_decode(VCAP_SERVICES, true);
-	$vcap_config = $service_json['mysql-5.1'][0]['credentials'];
+	$env_config = $service_json['mysql-5.1'][0]['credentials'];
+}
+elseif (getenv("TRAVIS") == true)
+{
+	$env_config['hostname'] = '127.0.0.1';
+	$env_config['username'] = 'travis';
+	$env_config['password'] = '';
+	$env_config['name']     = 'bpmppt_test';
 }
 
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = (isset($vcap_config['hostname']) ? $vcap_config['hostname'] : 'localhost');
-$db['default']['username'] = (isset($vcap_config['username']) ? $vcap_config['username'] : 'root');
-$db['default']['password'] = (isset($vcap_config['password']) ? $vcap_config['password'] : 'password');
-$db['default']['database'] = (isset($vcap_config['name']) ? $vcap_config['name'] : 'bpmppt_db');
+$db['default']['hostname'] = (isset($env_config['hostname']) ? $env_config['hostname'] : 'localhost');
+$db['default']['username'] = (isset($env_config['username']) ? $env_config['username'] : 'root');
+$db['default']['password'] = (isset($env_config['password']) ? $env_config['password'] : 'password');
+$db['default']['database'] = (isset($env_config['name']) ? $env_config['name'] : 'bpmppt_db');
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = 'baka_';
 $db['default']['pconnect'] = TRUE;
