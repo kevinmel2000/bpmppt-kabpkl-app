ad885a0b058e5fd6d58eb9567d1db25be215aa35
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-05-16 06:18:32 +0700

    update panel_alldata views

--- application/views/pages/panel_alldata.php
+++ application/views/pages/panel_alldata.php
@@ -3,7 +3,7 @@
 
 		<?php load_view('toolbar') ?>
 		<section id="dashboard-callout" class="row">
-		<?php $widget_col = twbs_set_columns(6, 6, 6, 12) ?>
+		<?php $widget_col = twbs_set_columns(6, 6, 6, 6, 12) ?>
 
 			<div class="<?php echo $widget_col ?>">
 			<?php $i = 1; $c = count($panel_body); ?>
@@ -15,7 +15,7 @@
 					</header>
 					<div class="panel-body">
 						<div class="row">
-							<div class="<?php echo twbs_set_columns(4, 4, 4) ?>">
+							<div class="<?php echo twbs_set_columns(4, 4, 4, 4, 12) ?>">
 								<p>Total Data</p>
 								<?php $chart_attr = array(
 									'id'             => $link.'-chart',
@@ -30,7 +30,7 @@
 								<canvas <?php echo parse_attrs($chart_attr) ?>></canvas>
 								<h1 class="total-layanan"><?php echo $izin['total'] ?></h1>
 							</div>
-							<div class="<?php echo twbs_set_columns(8, 8, 8) ?>">
+							<div class="<?php echo twbs_set_columns(8, 8, 8, 8, 12) ?>">
 								<p class="text-warning"><?php echo $izin['pending'] ?> Tertunda</p>
 								<p class="text-primary"><?php echo $izin['approved'] ?> Disetujui</p>
 								<p class="text-success"><?php echo $izin['done'] ?> Selesai</p>
