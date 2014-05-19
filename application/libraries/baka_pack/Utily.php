1053c2a8cf72b09cdfdb75d2b7e4a07a9dc7dc86
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-05-19 20:49:44 +0700

    change Baka Pack License to DBAD @philsturgeon/dbad :+1:

--- application/libraries/baka_pack/Utily.php
+++ application/libraries/baka_pack/Utily.php
@@ -1,23 +1,40 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * CodeIgniter Baka Pack
+ * DON'T BE A DICK PUBLIC LICENSE <http://dbad-license.org>
+ * 
+ * Version 0.1.4, May 2014
+ * Copyright (C) 2014 Fery Wardiyanto <ferywardiyanto@gmail.com>
+ *  
+ * Everyone is permitted to copy and distribute verbatim or modified copies of
+ * this license document, and changing it is allowed as long as the name is
+ * changed.
+ * 
+ * DON'T BE A DICK PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ * 
+ * 1. Do whatever you like with the original work, just don't be a dick.
+ * 
+ *    Being a dick includes - but is not limited to - the following instances:
+ * 
+ *    1a. Outright copyright infringement - Don't just copy this and change the name.  
+ *    1b. Selling the unmodified original with no work done what-so-ever,
+ *        that's REALLY being a dick.  
+ *    1c. Modifying the original work to contain hidden harmful content.
+ *        That would make you a PROPER dick.  
+ * 
+ * 2. If you become rich through modifications, related works/services, or
+ *    supporting the original work, share the love. Only a dick would make loads
+ *    off this work and not buy the original work's creator(s) a pint.
+ * 
+ * 3. Code is provided with no warranty. Using somebody else's code and bitching
+ *    when it goes wrong makes you a DONKEY dick. Fix the problem yourself.
+ *    A non-dick would submit the fix back.
  *
- * My very own Codeigniter Boilerplate Library that used on all of my projects
- * 
- * NOTICE OF LICENSE
- * 
- * Licensed under the Open Software License version 3.0
- * 
- * This source file is subject to the Open Software License (OSL 3.0) that is
- * bundled with this package in the files license.txt / license.rst.  It is
- * also available through the world wide web at this URL:
- * http://opensource.org/licenses/OSL-3.0
- *
- * @package     Baka_pack
+ * @package     CodeIgniter Baka Pack
  * @author      Fery Wardiyanto
- * @copyright   Copyright (c) Fery Wardiyanto. (ferywardiyanto@gmail.com)
- * @license     http://opensource.org/licenses/OSL-3.0
+ * @copyright   Copyright (c) Fery Wardiyanto. <ferywardiyanto@gmail.com>
+ * @license     http://dbad-license.org
  * @version     Version 0.1.4
  * @since       Version 0.1.0
  */
