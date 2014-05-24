962ee4be0888bf47064cce9e4aee8724c42ef45b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-05-25 00:31:02 +0700

    simplify error controller

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -71,104 +71,10 @@ class Error extends BAKA_Controller
 
     public function notice( $page = '' )
     {
-        switch( $page )
-        {   
-            // Registration
-            case '404':
-                $page_title     = '404 Halaman tidak ditemukan';
-                $page_message   = 'The page you requested was not found.';
-                break;
+        $page = str_replace('-', '_', $page);
 
-            // Registration
-            case 'registration-success':
-                $page_title     = 'Successful Registration';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, at animi error porro alias nesciunt rem explicabo a vitae voluptas officiis sint delectus blanditiis repellat velit voluptatum natus dolor amet! ';
-                break;
-            case 'registration-disabled':
-                $page_title     = 'Registration Disabled';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, suscipit, dicta sint tempore accusantium unde assumenda autem fugiat adipisci molestiae sequi praesentium soluta consequatur facilis similique blanditiis non et perferendis. ';
-                break;
-
-            // Activation
-            case 'activation-sent':
-                $page_title     = 'Activation Email Sent';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, distinctio, accusamus, aut repellendus odio numquam est quos incidunt quaerat magni facere labore mollitia qui natus asperiores beatae quas sed ut. ';
-                break;
-            case 'activation-complete':
-                $page_title     = 'Activation Complete';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, voluptates, laboriosam, in, quae quos excepturi nobis non aperiam tempora labore reiciendis temporibus a rem eos explicabo? Distinctio voluptatibus voluptas recusandae. ';
-                break;
-            case 'activation-failed':
-                $page_title     = 'Activation Failed';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi voluptatem facere repudiandae aliquam saepe. Quidem, omnis, cum excepturi autem alias iusto fugit ea similique ad sed necessitatibus veniam odit laboriosam. ';
-                break;
-
-            // Password
-            case 'password-changed':
-                $page_title     = 'Password Changed';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, rem, expedita, error excepturi a quis velit sunt tempore omnis illum quisquam facilis. Veritatis, aspernatur, fugit voluptatibus eum alias est aliquam! ';
-                break;
-            case 'password-sent':
-                $page_title     = 'New Password Sent';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, repellat, quidem, accusantium cupiditate alias corrupti deleniti tempora aliquid impedit vel rem porro sapiente pariatur nesciunt doloribus dolores harum? Doloribus, magnam! ';
-                break;
-            case 'password-reset':
-                $page_title     = 'Password Reset';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, quidem, ad vel rerum dolorem alias consequatur dolorum quisquam voluptatibus officiis excepturi neque optio ea reiciendis temporibus nemo dignissimos voluptas unde. ';
-                break;
-            case 'password-failed':
-                $page_title     = 'Password Failed';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, accusamus, vitae, voluptates ea nostrum maxime tenetur dolores cupiditate perspiciatis perferendis nobis facere accusantium totam incidunt optio. Repudiandae id beatae praesentium. ';
-                break;
-
-            // Email
-            case 'email-sent':
-                $page_title     = 'Confirmation Email Sent';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, voluptatum, voluptatibus mollitia ex blanditiis obcaecati debitis laudantium odio ipsam aut rem minima quod tenetur nostrum quisquam facilis voluptatem architecto fuga. ';
-                break;
-            case 'email-activated':
-                $page_title     = 'Your Email has been Activated';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, minus dignissimos ipsa consequuntur praesentium dolor qui placeat doloremque reprehenderit voluptatum neque fuga facilis accusantium velit laborum eveniet asperiores quod id. ';
-                break;
-            case 'email-failed':
-                $page_title     = 'Email Sending Failed';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, vel, ratione, accusamus, ex similique iste dolores officiis recusandae omnis quas odit debitis quaerat sit magnam numquam consequuntur deserunt? Autem, repudiandae. ';
-                break;
-
-            // User + Account
-            case 'user-banned':
-                $page_title     = 'You have been Banned.';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, similique vitae deleniti iure natus beatae dolorum minus officia maxime libero possimus praesentium quos atque aperiam recusandae unde velit culpa assumenda. ';
-                break;
-            case 'user-deleted':
-                $page_title     = 'Your account has been Deleted.';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, reprehenderit, commodi, mollitia nemo sequi esse consectetur vitae tenetur autem minus alias deleniti saepe et tempore cum sunt at dolorem iure! ';
-                break;
-            case 'acct-unapproved':
-                $page_title     = 'Account not yet Approved';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sit quo laborum perspiciatis magnam placeat fugiat sed eligendi ipsa dolorem. Quo, in minus sint delectus necessitatibus alias nesciunt incidunt natus? ';
-                break;
-            case 'logout-success':
-                $page_title     = 'Logged Out';
-                $page_message   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere commodi amet odit velit obcaecati ullam accusantium quia minima! Voluptatibus mollitia tempora veniam nihil quos quis explicabo quia deserunt asperiores cupiditate. ';
-                break;
-
-            case 'access-denied':
-                $page_title     = 'Oops! Anda tidak diperbolehkan mengakses halaman ini.';
-                $page_message   = 'Maaf, sepertinya administrator tidak memperbolehkan anda mengakses halaman ini. Jika ini merupakan suatu kesalahan, silahkan hubungi administrator terkait.';
-                break;
-            case 'no-data-accessible':
-                $page_title     = 'Oops! Tidak ada data untuk anda.';
-                $page_message   = 'Maaf, sepertinya administrator tidak memperbolehkan anda melihat satu data-pun dihalaman ini. Jika ini merupakan suatu kesalahan, silahkan hubungi administrator terkait.';
-                break;
-
-            default:
-                redirect('dashboard');
-                break;
-        }
-
-        $this->set_panel_title( $page_title );
-        $this->set_panel_body( $page_message );
+        $this->set_panel_title( _x('notice_'.$page.'_title') );
+        $this->set_panel_body( _x('notice_'.$page.'_message') );
 
         $this->load->theme('pages/notice', $this->data);
     }
--- application/language/indonesian/baka_pack_lang.php
+++ application/language/indonesian/baka_pack_lang.php
@@ -166,6 +166,60 @@ $lang['median_file_size']                         = 'File Size:';
 $lang['median_file_type']                         = 'File Type:';
 $lang['median_file_path']                         = 'Upload Destination:';
 
+// -----------------------------------------------------------------------------
+// Notice Message
+// -----------------------------------------------------------------------------
+// Registration
+$lang['notice_404_title']                     = '404 Halaman tidak ditemukan';
+$lang['notice_404_message']                   = 'The page you requested was not found.';
+
+// Registration
+$lang['notice_registration_success_title']    = 'Successful Registration';
+$lang['notice_registration_success_message']  = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, at animi error porro alias nesciunt rem explicabo a vitae voluptas officiis sint delectus blanditiis repellat velit voluptatum natus dolor amet! ';
+$lang['notice_registration_disabled_title']   = 'Registration Disabled';
+$lang['notice_registration_disabled_message'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, suscipit, dicta sint tempore accusantium unde assumenda autem fugiat adipisci molestiae sequi praesentium soluta consequatur facilis similique blanditiis non et perferendis. ';
+
+// Activation
+$lang['notice_activation_sent_title']         = 'Activation Email Sent';
+$lang['notice_activation_sent_message']       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, distinctio, accusamus, aut repellendus odio numquam est quos incidunt quaerat magni facere labore mollitia qui natus asperiores beatae quas sed ut. ';
+$lang['notice_activation_complete_title']     = 'Activation Complete';
+$lang['notice_activation_complete_message']   = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, voluptates, laboriosam, in, quae quos excepturi nobis non aperiam tempora labore reiciendis temporibus a rem eos explicabo? Distinctio voluptatibus voluptas recusandae. ';
+$lang['notice_activation_failed_title']       = 'Activation Failed';
+$lang['notice_activation_failed_message']     = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi voluptatem facere repudiandae aliquam saepe. Quidem, omnis, cum excepturi autem alias iusto fugit ea similique ad sed necessitatibus veniam odit laboriosam. ';
+
+// Password
+$lang['notice_password_changed_title']        = 'Password Changed';
+$lang['notice_password_changed_message']      = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, rem, expedita, error excepturi a quis velit sunt tempore omnis illum quisquam facilis. Veritatis, aspernatur, fugit voluptatibus eum alias est aliquam! ';
+$lang['notice_password_sent_title']           = 'New Password Sent';
+$lang['notice_password_sent_message']         = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, repellat, quidem, accusantium cupiditate alias corrupti deleniti tempora aliquid impedit vel rem porro sapiente pariatur nesciunt doloribus dolores harum? Doloribus, magnam! ';
+$lang['notice_password_reset_title']          = 'Password Reset';
+$lang['notice_password_reset_message']        = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, quidem, ad vel rerum dolorem alias consequatur dolorum quisquam voluptatibus officiis excepturi neque optio ea reiciendis temporibus nemo dignissimos voluptas unde. ';
+$lang['notice_password_failed_title']         = 'Password Failed';
+$lang['notice_password_failed_message']       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, accusamus, vitae, voluptates ea nostrum maxime tenetur dolores cupiditate perspiciatis perferendis nobis facere accusantium totam incidunt optio. Repudiandae id beatae praesentium. ';
+
+// Email
+$lang['notice_email_sent_title']              = 'Confirmation Email Sent';
+$lang['notice_email_sent_message']            = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, voluptatum, voluptatibus mollitia ex blanditiis obcaecati debitis laudantium odio ipsam aut rem minima quod tenetur nostrum quisquam facilis voluptatem architecto fuga. ';
+$lang['notice_email_activated_title']         = 'Your Email has been Activated';
+$lang['notice_email_activated_message']       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, minus dignissimos ipsa consequuntur praesentium dolor qui placeat doloremque reprehenderit voluptatum neque fuga facilis accusantium velit laborum eveniet asperiores quod id. ';
+$lang['notice_email_failed_title']            = 'Email Sending Failed';
+$lang['notice_email_failed_message']          = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, vel, ratione, accusamus, ex similique iste dolores officiis recusandae omnis quas odit debitis quaerat sit magnam numquam consequuntur deserunt? Autem, repudiandae. ';
+
+// User + Account
+$lang['notice_user_banned_title']             = 'You have been Banned.';
+$lang['notice_user_banned_message']           = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, similique vitae deleniti iure natus beatae dolorum minus officia maxime libero possimus praesentium quos atque aperiam recusandae unde velit culpa assumenda. ';
+$lang['notice_user_deleted_title']            = 'Your account has been Deleted.';
+$lang['notice_user_deleted_message']          = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, reprehenderit, commodi, mollitia nemo sequi esse consectetur vitae tenetur autem minus alias deleniti saepe et tempore cum sunt at dolorem iure! ';
+$lang['notice_acct_unapproved_title']         = 'Account not yet Approved';
+$lang['notice_acct_unapproved_message']       = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sit quo laborum perspiciatis magnam placeat fugiat sed eligendi ipsa dolorem. Quo, in minus sint delectus necessitatibus alias nesciunt incidunt natus? ';
+$lang['notice_logout_success_title']          = 'Logged Out';
+$lang['notice_logout_success_message']        = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere commodi amet odit velit obcaecati ullam accusantium quia minima! Voluptatibus mollitia tempora veniam nihil quos quis explicabo quia deserunt asperiores cupiditate. ';
+
+$lang['notice_access_denied_title']           = 'Oops! Anda tidak diperbolehkan mengakses halaman ini.';
+$lang['notice_access_denied_message']         = 'Maaf, sepertinya administrator tidak memperbolehkan anda mengakses halaman ini. Jika ini merupakan suatu kesalahan, silahkan hubungi administrator terkait.';
+$lang['notice_no_data_accessible_title']      = 'Oops! Tidak ada data untuk anda.';
+$lang['notice_no_data_accessible_message']    = 'Maaf, sepertinya administrator tidak memperbolehkan anda melihat satu data-pun dihalaman ini. Jika ini merupakan suatu kesalahan, silahkan hubungi administrator terkait.';
+
 
 /* End of file baka_pack_lang.php */
 /* Location: ./application/language/indonesian/baka_pack_lang.php */
