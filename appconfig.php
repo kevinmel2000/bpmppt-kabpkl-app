1e6d60df97a1c432c249bfdce907729486c43c01
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-06-20 10:06:25 +0700

    litle configuration patch

--- appconfig.php
+++ appconfig.php
@@ -1,20 +1,19 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-if (VCAP_SERVICES)
-{
-	$service_json = json_decode(VCAP_SERVICES, true);
-	$env_config   = $service_json['mysql-5.1'][0]['credentials'];
-}
-elseif (getenv("TRAVIS") == true)
-{
-	$env_config['hostname'] = '127.0.0.1';
-	$env_config['username'] = 'travis';
-	$env_config['password'] = '';
-	$env_config['name']     = 'bpmppt_test';
-}
+/*
+|--------------------------------------------------------------------
+| BASE SITE URL
+|--------------------------------------------------------------------
+*/
+define('APP_BASE_URL', 'http://localhost/');
 
-define('APP_HOSTNAME', isset($env_config['hostname']) ? $env_config['hostname'] : 'localhost');
-define('APP_USERNAME', isset($env_config['username']) ? $env_config['username'] : 'root');
-define('APP_PASSWORD', isset($env_config['password']) ? $env_config['password'] : 'password');
-define('APP_DATABASE', isset($env_config['name'])     ? $env_config['name']     : 'bpmppt');
+/*
+| -------------------------------------------------------------------
+| DATABASE CONNECTIVITY SETTINGS
+| -------------------------------------------------------------------
+*/
+define('APP_HOSTNAME', 'localhost');
+define('APP_USERNAME', 'root');
+define('APP_PASSWORD', 'password');
+define('APP_DATABASE', 'bpmppt');
 define('APP_DBPREFIX', 'baka_');
