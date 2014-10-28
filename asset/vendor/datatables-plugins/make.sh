6b0ce7911a7e176c84eba5e342c21fd2eb20ac72
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-10-28 11:54:45 +0700

    Update vendor

--- asset/vendor/datatables-plugins/make.sh
+++ asset/vendor/datatables-plugins/make.sh
@@ -10,8 +10,11 @@ DT_SRC=$(dirname $(dirname $(pwd)))
 DT_BUILT="${DT_SRC}/built/DataTables"
 . $DT_SRC/build/include.sh
 
+scss_compile $DT_SRC/extensions/Plugins/integration/jqueryui/dataTables.jqueryui.scss
+
+js_compress $DT_SRC/extensions/Plugins/features/searchHighlight/dataTables.searchHighlight.js
+js_compress $DT_SRC/extensions/Plugins/features/alphabetSearch/dataTables.alphabetSearch.js
+
 # Only copying the integration files
 rsync -r integration     $OUT_DIR
-
-scss_compile $OUT_DIR/integration/jqueryui/dataTables.jqueryui.scss
 
