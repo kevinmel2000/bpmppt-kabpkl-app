8a54b31dfb8c23e244d797a3a080eeb97a6f6251
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-10-29 10:03:54 +0700

    refix images encoding

--- Gruntfile.js
+++ Gruntfile.js
@@ -290,7 +290,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'asset/vendor',
-          src: [ '**/*.{js,css,less}' ],
+          src: [ '**/*.{js,css,svg,less}' ],
           dest: 'asset/vendor/'
         }]
       }
